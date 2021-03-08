e70ff21586ce8e6d4c0e386824870e4824fc8c94
Kingsley <kingobaino@gmail.com>
2021-03-08 18:23:48 +0100

    Rectify the issue with with fetching data

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ const scroll = () => {
 };
 
 async function data(city){
-  const url = fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=700bca0642ce1cbc0f9e5ca93c0ea7ef`, { mode: 'cors' })
+  const url = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=700bca0642ce1cbc0f9e5ca93c0ea7ef`, { mode: 'cors' })
   const responds = await url;
   const result = responds.json();
   const value = await result;
